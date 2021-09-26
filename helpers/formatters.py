46b0f76c0883c72c84ee3a3a26498fc070329c6f
snikidev <n.kakuev@gmail.com>
2021-09-26 21:38:46 +0100

    Move away from commercejs and s3 to airtable

--- helpers/formatters.py
+++ helpers/formatters.py
@@ -1,12 +1,3 @@
-import re
-
-
-def html_to_string_formatter(text):
-    cleanr = re.compile("<.*?>")
-    cleantext = re.sub(cleanr, "", text)
-    return cleantext
-
-
 def escape_markdown_chars(text):
     return (
         text.replace("_", "\\_")
--- models/product.py
+++ models/product.py
@@ -1,7 +1,6 @@
 from pydantic import BaseModel, Field, root_validator
 from common.settings import Settings
 from telebot.types import LabeledPrice
-from helpers.formatters import html_to_string_formatter
 
 settings = Settings()
 
@@ -9,6 +8,7 @@ settings = Settings()
 class Product(BaseModel):
     currency: str = "gbp"
     description: str
+    title: str
     invoice_payload: str = Field(alias="id")
     is_flexible: bool = False  # True If you need to set up Shipping Fee
     need_email: bool = True
@@ -20,17 +20,30 @@ class Product(BaseModel):
     provider_token: str = settings.stripe_token
     send_email_to_provider: bool = True
     start_parameter: str = Field(alias="id")
-    title: str = Field(alias="name")
 
     @root_validator(pre=True)
     def get_nested_values(cls, values):
-        photo_url = values.get("media").get("source").replace(" ", "%20")
+        title, price, thumbnail_large = (
+            values.get("title"),
+            values.get("price"),
+            values.get("photo")[0].get("thumbnails").get("large"),
+        )
+
+        photo_url = thumbnail_large.get("url").replace(" ", "%20")
+        photo_height = thumbnail_large.get("height")
+        photo_width = thumbnail_large.get("width")
+
         prices = [
             LabeledPrice(
-                label=str(values.get("name")),
-                amount=int(values.get("price").get("raw") * 100),
+                label=str(title),
+                amount=int(price * 100),
             )
         ]
-        description = html_to_string_formatter(values.get("description"))
 
-        return dict(values, photo_url=photo_url, prices=prices, description=description)
+        return dict(
+            values,
+            photo_url=photo_url,
+            prices=prices,
+            photo_height=photo_height,
+            photo_width=photo_width,
+        )
